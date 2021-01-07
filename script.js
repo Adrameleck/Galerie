5f1578d19e9b8565736c75567a41541b79e12f93
Guillaume <guillaume.navarro@ynov.com>
2021-01-07 09:59:10 +0100

    Correction exercice 2

--- script.js
+++ script.js
@@ -1,10 +1,92 @@
-console.log("JAVASCRIPT LOADED");
-
-
-var listeImages = document.getElementById("images");
-for(let i = 0; i < 5 ; i++){
-    var image1 = document.createElement("img");
-    image1.setAttribute("class", "image");
-    image1.setAttribute("src", 'D:\\Guillaume\\Travail Ynov\\M2\\Web_Mobile\\exo1\\resources\\0.jpg');
-    listeImages.append(image1);
+function reduireArray(array, size) {
+  if (array.length <= size) {
+    return [array];
+  }
+  return [array.slice(0, size), ...reduireArray(array.slice(size), size)];
 }
+
+const dateTimeFormat = Intl.DateTimeFormat("fr");
+
+function afficher(json){
+	const selections = reduireArray(json, 4);
+
+  let html = "";
+
+  selections.forEach(selection => {
+    html += '<div class="columns">';
+
+    selection.forEach(repo => {
+      html += `
+            <div class="column">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img
+                    src="${repo.url}"
+                    alt="Placeholder image"
+                  />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img
+                        src="https://giffiles.alphacoders.com/981/98174.gif"
+                        alt="Placeholder image"
+                      />
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">${repo.name}</p>
+                    <p class="subtitle is-6">@Parcourir</p>
+                  </div>
+                </div>
+  
+                <div class="content">
+                   ${repo.description}
+                  <br />
+                  Dernière mise à jour: <time datetime="${
+                    repo.updated_at
+                  }">${dateTimeFormat.format(new Date(repo.updated_at))}</time>
+                </div>
+              </div>
+            </div>
+          </div>`;
+    });
+    html += "</div>";
+  });
+
+  document.querySelector(".container").innerHTML = html;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("https://nostalgic-lamarr-5a666c.netlify.app/images.json")
+    .then((response) => response.json())
+    .then((json) => afficher(json));  
+});
+
+window.addEventListener('beforeinstallprompt', e => { 
+  e.preventDefault() ; 
+  deferredPrompt = e ; 
+  const btn = document.getElementById('button') ; 
+ 
+  btn.addEventListener('click', e  =>{ 
+    deferredPrompt.prompt() ; 
+ 
+    deferredPrompt.userChoice 
+      .then((choiceResult) => { 
+         if (choiceResult.outcome === 'accepted') { 
+           console.log('A2HS prompt accepté'); 
+         } else { 
+            console.log('A2HS prompt décliné'); 
+       } 
+    deferredPrompt = null; 
+   }); 
+  }) ; 
+}) ;
+
+window.addEventListener('appinstalled', e => { 
+   console.log('application installée') ; 
+}) ;
+
